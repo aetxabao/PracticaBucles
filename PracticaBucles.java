93cf7aaefe02533c152c2e3219abe462d0eee7f3
evirunurm <teakid21@gmail.com>
2021-11-19 10:33:50 +0100

    esImpar() method finished

--- PracticaBucles.java
+++ PracticaBucles.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 /**
- *    
+ *     @author Evelin Virunurm
  */
 public class PracticaBucles {
     private final char ESPACIO = ' ';
@@ -40,10 +40,7 @@ public class PracticaBucles {
      *  Hazlo sin utilizar if
      */
     public boolean esImpar(int numero)   {
-        //TODO
-        
-        
-        return  false;
+        return (numero % 2 == 0);
     }
 
     /**
